247feb405cb12b5750c684f60e348a12bcd1da0b
0xAubrey <0xAubrey@users.noreply.github.com>
2019-04-22 14:51:08 +0100

    mailchip wire up

--- src/sections/main-content.js
+++ src/sections/main-content.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import logo from '../logo.svg';
 
+const TELEGRAM_URL = "http://t.me/TDupdater_bot";
+
 export const MainContent = () => {
     return (
         <div className="td-trader-main-content">
@@ -17,10 +19,10 @@ export const MainContent = () => {
                   TDtrader is a Telegram bot that keeps you up to date on the current TD count.
                   You simply get a notification on every candle close.
                 </p>
-                <button className="btn btn-primary">
+                <a href={TELEGRAM_URL} className="btn btn-primary">
                   Pair with Telegram
                   <span><i className="fab fa-telegram-plane"></i></span>
-                </button>
+                </a>
               </div>
               <div className="col col-lg-5 d-none d-lg-block">
                 <img src={logo} className="td-trader-main-content__logo" alt="logo" />
